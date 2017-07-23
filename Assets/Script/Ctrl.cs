1047ddf35b0e277209b87b971aa104935f8356dc
蔡华 <huacai@outlook.com>
2017-07-24 01:44:56 +0800

    Create
    first commit，ignore library and temp

---
+++ Assets/Script/Ctrl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+[LuaCallCSharp]
+public class Ctrl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        StartCoroutine(foo());
+    }
+
+    private IEnumerator foo()
+    {
+        yield return new WaitForSeconds(3f);
+        GameRoot.Instance.luaEnv.DoString("print(CubeCtrl.a)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+}
---
+++ Assets/Script/GameRoot.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using XLua;
+
+public class GameRoot : MonoBehaviour
+{
+    public static GameRoot Instance;
+    public LuaEnv luaEnv;
+    void Awake()
+    {
+        Instance = this;
+        luaEnv = new LuaEnv();
+    }
+    // Use this for initialization
+    void Start()
+    {
+        LoadLua();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void LoadLua()
+    {
+        AssetBundle bundle = AssetBundle.LoadFromFile("Assets/StreamingAssets/lua");
+        //luaEnv.AddLoader((ref string path) =>
+        //{
+        //    if (path == "Main")
+        //    {
+        //        TextAsset lua = bundle.LoadAsset("Main.lua", typeof(TextAsset)) as TextAsset;
+        //        return lua.bytes;
+        //    }
+        //    return null;
+        //});
+        luaEnv.AddLoader((ref string path) =>
+        {
+            TextAsset lua = bundle.LoadAsset(path + ".lua", typeof(TextAsset)) as TextAsset;
+            return lua.bytes;
+        });
+        luaEnv.DoString("require 'Main'   print(Main.a)");
+        //luaEnv.DoString("print(CubeCtrl.a)");
+    }
+
+    IEnumerator GetLuaScript()
+    {
+
+        UnityWebRequest www = UnityWebRequest.GetAssetBundle("file:///C:/Users/Sai/Desktop/AssetBundle/lua", 1, 1);
+        yield return www.Send();
+
+        if (www.isNetworkError)
+        {
+            yield return www.error;
+        }
+        else
+        {
+            AssetBundle bundle = ((DownloadHandlerAssetBundle)www.downloadHandler).assetBundle;
+            TextAsset[] luascripts = bundle.LoadAllAssets<TextAsset>();
+            yield return luascripts;
+            //TextAsset CubeCtr = bundle.LoadAsset("Main.lua", typeof(TextAsset)) as TextAsset;
+            //yield return CubeCtr.bytes;
+        }
+    }
+}
